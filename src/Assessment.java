4044a3eba7d19805097518c38aebab08526b0d85
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-09 11:00:58 -0500

    Java Assessment

---
+++ src/Assessment.java
@@ -0,0 +1,9 @@
+public class Assessment {
+    public static int main(String[] args) {
+        public static int square{
+            int num = 1;
+            int total = num * num;
+            return (total);
+        }
+    }
+}
